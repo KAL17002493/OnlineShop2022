313cc9001ec8f16ed3a2c4b3d1b14ee52a91effd
KAL17002493 <74010320+KAL17002493@users.noreply.github.com>
2022-01-06 14:48:13 +0000

    Login and Register features implemented

--- OnlineShop2022/Migrations/20220106140915_user.Designer.cs
+++ OnlineShop2022/Migrations/20220106140915_user.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineShop2022.Data;
 
 namespace OnlineShop2022.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220106140915_user")]
+    partial class user
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OnlineShop2022/Models/CustomUserModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop2022.Models
+{
+    public class CustomUserModel : IdentityUser
+    {
+        [Required]
+        public string FName { get; set; }
+        [Required]
+        public string SName { get; set; }
+    }
+}
